c2a7185c3d671138feaaf97ce36f461ecce2880c
fabricio-Viana <fabricio.viana.2301@gmail.com>
2024-12-14 17:12:10 -0300

    Objeto em javascript (bem diferente de java)

---
+++ Fundamentos/11_Objeto.js
@@ -0,0 +1,17 @@
+let prod1 = {}
+
+prod1.nome = "maquina de lavar"
+prod1.preco = 2500
+prod1['deconto legal'] = 200 // não criar atributos com espaço
+
+console.log(prod1);
+
+let produto2 = {
+    nome: 'camisa polo',
+    preco: 49.90,
+    obj:{
+        blabla: 2 // podemos ter um objeto dentro de um objeto
+    }
+}
+console.log(produto2)
+
