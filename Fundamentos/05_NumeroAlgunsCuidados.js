f67af3745f94c4986444bde49f3f12b78658db15
fabricio-Viana <fabricio.viana.2301@gmail.com>
2024-12-14 00:43:55 -0300

    Cuidados com os tipos numerico

---
+++ Fundamentos/05_NumeroAlgunsCuidados.js
@@ -0,0 +1,6 @@
+console.log(7/0);
+console.log("10"/2); //tipo string dividido para tipo inteiro quando ele reconhece como numero 
+console.log("Hello World" * 2) // NaN significa  não é um numero
+console.log(0.1 + 0.7);
+// console.log(10.toString) não da para fazer isto com numero literal
+console.log((5.3839).toFixed(1))
