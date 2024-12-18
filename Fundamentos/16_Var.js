2889eacad91f9c1e1b657408639cfbe6db9afc2d
fabricio-Viana <fabricio.viana.2301@gmail.com>
2024-12-17 23:32:05 -0300

    Var

---
+++ Fundamentos/16_Var.js
@@ -0,0 +1,10 @@
+{ { { { { { { { { var sera = "SERA?" } } } } } } } } } // var torna ela visivel num escopo globaç
+
+console.log(sera)
+
+function test(){
+    var local =123
+    console.log(local)
+
+}
+test();
