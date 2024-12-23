8b358dde85c93344efb2c731a8d596264cec1482
fabricio-Viana <fabricio.viana.2301@gmail.com>
2024-12-23 16:52:11 -0300

    if_2

---
+++ Controle/02_if2.js
@@ -0,0 +1,18 @@
+function test1(num){
+    if(num > 7){
+        console.log(num)
+        console.log('final')
+    }
+}
+
+test1(9)
+test1(6)
+
+function test2(num){
+    if(num > 7);
+    {
+        console.log(num)
+    }
+}
+test2(2)
+test2(19)
