420eec88de49c7d4f5e1008dd4a6b24c48645271
fabricio-Viana <fabricio.viana.2301@gmail.com>
2024-12-24 00:27:30 -0300

    For

---
+++ Controle/08_for.js
@@ -0,0 +1,21 @@
+let contador = 0 
+
+while(contador <= 10){
+    console.log(`Contador = ${contador}`)
+    contador++
+}
+
+console.log('-------------------------')
+// agora com o for
+for(let i = 0; i <=10 ;i++){
+    console.log(`i = ${i}`)
+
+}
+console.log('-------------------------')
+
+const notas = [7.5, 8.4, 5.5, 8.0 ,10.0]
+
+for(let i =0 ; i < notas.length ;i++){
+    console.log(`nota = ${notas[i]}`)
+}
+
