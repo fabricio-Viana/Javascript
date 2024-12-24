bdb3c70f9110638917170aed8b6aee602610d04a
fabricio-Viana <fabricio.viana.2301@gmail.com>
2024-12-23 22:19:50 -0300

    While

---
+++ Controle/06_While.js
@@ -0,0 +1,19 @@
+let nota = 0
+
+while (true) {
+    console.log('Hello World')
+    nota++
+    if (nota == 15) {
+        break
+    }
+}
+console.log(nota)
+
+//--------------------------------
+let n = 0
+
+while (n <= 15) {
+    console.log('Hello World'.toLocaleUpperCase())
+    n++
+}
+console.log(n.valueOf())
