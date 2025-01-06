30b10a46a36c8af4d9c6ff675940d3c191977ebd
fabricio-Viana <fabricio.viana.2301@gmail.com>
2025-01-06 12:42:33 -0300

    arrow function

---
+++ 03_Função/07_ArrowFunction01.js
@@ -0,0 +1,14 @@
+const f1 = (n1,n2) => {return console.log(n1 + n2)}
+
+f1(3,2)
+
+const dobro = function(n){
+    return 2*n
+}
+
+const dobro2 = (n)=>{return 2*n}
+
+const ola = () => "ola"
+
+console.log(dobro2(10))
+console.log(ola())
