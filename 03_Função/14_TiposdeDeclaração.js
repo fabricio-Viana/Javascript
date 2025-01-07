e60402c0b4ccf064a06c8aca0252be5a9c69f4e8
fabricio-Viana <fabricio.viana.2301@gmail.com>
2025-01-07 13:12:22 -0300

    Tipos de Declaração de função

---
+++ 03_Função/14_TiposdeDeclaração.js
@@ -0,0 +1,27 @@
+// tipo function
+
+console.log(n1(3,1))
+
+function n1(x,y){  //// pode ser chamada antees
+    return x+y;
+}
+
+// function exprection
+
+let soma =  function n1(x,y){ // não pode ser chamada antes apenas depois
+    return x+y;
+}
+
+console.log(soma(3,9))
+
+// named function expression
+
+
+let conta = function conta(x,y){
+    return x+y;
+}
+
+console.log(soma(6,1))
+
+
+
