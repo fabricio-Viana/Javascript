2d992da79ac4f51c7c1e27f5552ef6e90aa8e251
fabricio-Viana <fabricio.viana.2301@gmail.com>
2025-01-10 00:11:46 -0300

    Herança 1

---
+++ 04_Objeto/07_Herança1.js
@@ -0,0 +1,22 @@
+const ferrari ={
+    modelo: "F40",
+    velMax: 324
+}
+
+const Volvo  ={
+    modelo: "V40",
+    velMax: 200
+} 
+
+
+console.log(ferrari.__proto__)
+console.log(ferrari.__proto__=== Object.prototype)  
+console.log(Volvo.__proto__=== Object.prototype)  
+
+console.log(Object.prototype.__proto__)
+
+
+function meuobj(){}
+
+console.log(typeof Object, typeof meuobj)
+console.log(Object.prototype,meuobj.prototype)
