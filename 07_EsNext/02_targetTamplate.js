2d2c44947b222c87c4e87f7babc965990a1b7fb9
fabricio-Viana <fabricio.viana.2301@gmail.com>
2025-01-23 15:38:22 -0300

    Tamplate string em função

---
+++ 07_EsNext/02_targetTamplate.js
@@ -0,0 +1,10 @@
+function tag(partes, ...valores){
+    console.log(partes)
+    console.log(valores)
+    return 'Outra string'
+}
+
+const aluno = 'Fabricio'
+const situação = 'aprovado'
+
+console.log(`${aluno} esta ${situação}`)
