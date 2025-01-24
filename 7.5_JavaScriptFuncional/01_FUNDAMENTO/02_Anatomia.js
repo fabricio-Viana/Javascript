87b390aea1d2730e56818f41a6b0a4cfa79d7386
fabricio-Viana <fabricio.viana.2301@gmail.com>
2025-01-24 15:04:15 -0300

    Anatomia de uma função revisão

---
+++ 7.5_JavaScriptFuncional/01_FUNDAMENTO/02_Anatomia.js
@@ -0,0 +1,14 @@
+(function (a,b,c){
+    return 1+2+3
+}
+)
+
+const soma = function(a ,b){
+    return  a + b
+}
+
+console.log(soma(10,10))
+
+const outraSoma = soma
+
+console.log(outraSoma(234,37))
