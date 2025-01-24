ea96f5061d69cd2c9349a2eec9af0a8c01db2244
fabricio-Viana <fabricio.viana.2301@gmail.com>
2025-01-24 16:34:55 -0300

    anatomia de função 2 revisão

---
+++ 7.5_JavaScriptFuncional/01_FUNDAMENTO/03_Anatomia.js
@@ -0,0 +1,28 @@
+const incremento1 = function(n){
+        return n++
+}
+
+
+
+const incremento2 = (n)=>{
+    return n++
+}
+
+
+const incremento3 = n =>{
+    return n++
+}
+
+const incremento4 = n => n++
+
+const soma = (n,m) => n+m
+
+console.log(incremento1(1))
+console.log(incremento2(24))
+console.log(incremento3(50))
+console.log(incremento4(100))
+
+console.log(soma(250,250))
+console.log(soma('Fabricio ','Viana'))
+
+
---
+++ 7.5_JavaScriptFuncional/01_FUNDAMENTO/04_Anatomia.js
@@ -0,0 +1,11 @@
+(function (a,b,c){
+    let x = a+b+c
+    console.log(x)
+
+})(800,95,105);
+
+(function (nome, ...sobreNome){
+    let x = nome +" "+sobreNome
+    console.log(x)
+
+})('Fabricio','Viana')
