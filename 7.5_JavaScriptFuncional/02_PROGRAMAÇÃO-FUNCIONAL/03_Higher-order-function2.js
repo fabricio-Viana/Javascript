ca755be67a22bbd9486a650248252a1e6568ae82
fabricio-Viana <fabricio.viana.2301@gmail.com>
2025-02-03 23:39:41 -0300

    teste de função com while

--- 7.5_JavaScriptFuncional/02_PROGRAMAÇÃO-FUNCIONAL/03_Higher-order-function2.js
+++ 7.5_JavaScriptFuncional/02_PROGRAMAÇÃO-FUNCIONAL/03_Higher-order-function2.js
@@ -9,3 +9,16 @@ const BrFinalPrice = finalPrice(1)
 console.log(BrFinalPrice(25.1))
 console.log(BrFinalPrice(21.7))
 console.log(BrFinalPrice(107.9))
+
+function repetir(){
+    return function xVezes(nome){
+        x = 0
+        while(x<=10){
+            console.log(nome)
+            x++
+        }
+    }}
+
+    const repitaSeuNome = repetir()
+
+    repitaSeuNome("FABRICIO")
