51edff56854b07a655c9fa42361ed74a8e26eab0
fabricio-Viana <fabricio.viana.2301@gmail.com>
2025-02-04 01:54:35 -0300

    Formulario

---
+++ 08_HTML/exercicios/form/server.js
@@ -0,0 +1,21 @@
+const express = require("express")
+
+const app = express()
+
+const bodyParse = require('body-parser')
+
+app.use(bodyParse.urlencoded({extended: true}))
+
+app.post('/usuarios', (req,resp)=>{
+    console.log(req.body)
+    resp.send('<h1>parabens. usuario incluido</h1>')
+})
+app.post('/usuarios/:id', (req,resp)=>{
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('<h1>parabens. usuario alterado!</h1>')
+})
+
+
+app.listen(3003)
+
